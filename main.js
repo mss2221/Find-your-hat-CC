36061379fb5d192f58114f983835ff3ab2c0b776
mss <mss2221@columbia.edu>
2023-07-29 12:44:45 +0200

    added method to create field

--- main.js
+++ main.js
@@ -1,98 +1,102 @@
 const prompt = require('prompt-sync')({sigint: true});
-//const move = prompt('Please select direction to move; Up:u, Down:d, Left:l, Right:r ...')
 
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
-// 'list' is prob not best term to use in variables,
-// esp since it is a data type in Python. But I like
-// how we have it here, where 'array' refers to the whole 2-dimensional field.
+const getMove = () => {
+    move = prompt('Please select direction to move; Up:u, Down:d, Left:l, Right:r ...');
+    return move;
+  } 
+  
 class Field {
   constructor(){
     this.field = []
   };
-// changed .print() method to .generateField() to match Codecademy tasks
-// also Codecademy suggest making this a static function
-
-  selectCharacters(height,width) {
+  // changed .print() method to .generateField() to match Codecademy tasks
+  // Also, Codecademy suggest making this a static function
+  selectCharacters() {
+    console.log('Create playing field:')
+    const height = prompt('   select field height: ');
+    const width = prompt('   select field width: ')
     const characterList = [];
     const listLength = height * width;
-//    characterList.push('*'); // add '*' last
     characterList.push('^');
-// calculate percentage of Os (~2%?)
-  const totalHoles = parseInt(listLength * 0.2);
-  for (let i = 0; i < totalHoles; i++) {
-      characterList.push('O');
-  };
-// calculate number of fieldCaracters to add. minus holes, hat , asterisk
-  const totalFieldCharacters = listLength - totalHoles - 2
+  // calculate percentage of Os (~2%?)
+    const totalHoles = parseInt(listLength * 0.2);
+    for (let i = 0; i < totalHoles; i++) {
+        characterList.push('O');
+    };
+  // calculate number of fieldCaracters to add. minus holes, hat , asterisk
+    const totalFieldCharacters = listLength - totalHoles - 2
 
-// add fieldCharacters to list, leave space for * at beginning
-  for (let i = 0; i < totalFieldCharacters; i++) {
-    characterList.push('░')
-  }
-// randomize elements. This is from stack overflow
-// and to me was clearest method I found.
+  // add fieldCharacters to list, leave space for * at beginning
+    for (let i = 0; i < totalFieldCharacters; i++) {
+      characterList.push('░')
+    }
+  // randomize elements. This is from stack overflow
+  // and to me was clearest method I found.
+    for (let i = characterList.length - 1; i > 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      [characterList[i], characterList[rand]] = [characterList[rand], characterList[i]]
+    }
+  // add pathCharacter at start
+    characterList.splice(0,0,'*')
 
-  for (let i = characterList.length - 1; i > 0; i--) {
-    let rand = Math.floor(Math.random() * (i + 1));
-    [characterList[i], characterList[rand]] = [characterList[rand], characterList[i]]
-  }
-// add pathCharacter at start
-  characterList.splice(0,0,'*')
-
-// calculate number of elements that should be in each row
-// splice off elements from the original array and push into row
+  // calculate number of elements that should be in each row
+  // splice off elements from the original array and push into row
 
     let startingField = [];
     let row = [];
     let numberOfCharacters = characterList.length;
     let rowLength = numberOfCharacters/height;
     for (let i = 0; i < height; i++) {
-        const row = characterList.splice(0,rowLength);
-        startingField.push(row);
-      }
-//  now with the characterList broken up into rows (arrays)
-//  and pushed into startingField array:
-//  Loop through startingField for each row in height variable
+      const row = characterList.splice(0,rowLength);
+      startingField.push(row);
+    }
     return startingField
   }
   printField(field) {
     let height = field.length;
     for (let i = 0; i < height; i++) {
     console.log(field[i].join(''));
-  }
+    }
   }
 }
+
 
 // All our functions are methods on the Field class.
 // I am not sure I see the reason for this.
 // Perhaps Codecademy was thinking every move would create
 // a new instance of the Field class.
 // We are creating 1 field, and then mutating it. (which seems the simplest way to go)
-const myField = new Field();
-gameArray = myField.selectCharacters(4,5);
-myField.printField(gameArray);
+
+// Start Game
+
+// Setup field
+let newField = new Field();
+gameArray = newField.selectCharacters();
 
 
-// PLAY GAME!
-
+// initial conditions
 let foundHat= false;
 let foundHole= false;
 let pathPositionX = 0;
 let pathPositionY = 0;
 let pathPosition = gameArray[0][0];
 
+// PLAY GAME!
 
-// loop and compare '*' position
+// loop: get move, advance '*', evaluate position, redraw field
 while (!foundHat && !foundHole) {
-  const move = prompt('Please select direction to move; Up:u, Down:d, Left:l, Right:r ...');
+  // update field and print to console
+    gameArray[pathPositionY][pathPositionX] = pathCharacter;
+    newField.printField(gameArray);
 
-// starting coordinates
-  let oldX = pathPositionX;
-  let oldY = pathPositionY;
+  // I kept getting the prompt even after breaking out of loop. 
+  // So I tried putting it into a function and that seems to work.
+  move = getMove();
 
   switch(move) {
     case 'u':
@@ -108,30 +112,30 @@ while (!foundHat && !foundHole) {
       pathPositionX +=1;
       break;
     };
+ 
+ // check if move results in Win or Lose
 
-  // I removed `position = undefined` because I was getting unexpected behavior
-  // I changed something and then I was able to continue moving right across the field and beyond.
-  if  (pathPositionY >= gameArray.length || pathPositionY < 0 ) {
-        console.log('You fell out of the field!');
-        foundHole = true;
-        break;
-  }
-  if  (pathPositionX >= gameArray[0].length || pathPositionX < 0 ) {
-        console.log('You fell out of the field!');
-        foundHole = true;
-        break;
-  }
-  pathPosition = gameArray[pathPositionY][pathPositionX];
-  if (pathPosition === '^') {
-        console.log('Congratulations! You found your hat!');
-        foundHole = true;
-        break;
-  } else if (pathPosition === 'O') {
-    console.log('You fell into a hole!');
-    foundHole = true;
+ // break out of loop if x or y is negative
+ // before evaluating gameArray[x][y]
+ // (I removed `position = undefined` test because I was getting unexpected behavior)
+
+  if (pathPositionX < 0 || pathPositionY < 0) {
+    console.log('\nYou fell out of the field!');
+    break;
+  } else if (pathPositionX >= gameArray[0].length || pathPositionY >= gameArray.length) {
+    console.log('\nYou fell out of the field!');
     break;
   }
-// draw new array -
-  gameArray[pathPositionY][pathPositionX] = pathCharacter;
-  myField.printField(gameArray);
+
+   // now calculate the * position
+   pathPosition = gameArray[pathPositionY][pathPositionX];
+  if (pathPosition === '^') {
+    console.log('\nCongratulations! You found your hat!');
+    break;
+  } else if (pathPosition === 'O') {
+    console.log('\nYou fell into a hole!');
+    break;
+  }
+  pathPosition = pathCharacter;
 }
+console.log('\n    [  game over  ]') 
